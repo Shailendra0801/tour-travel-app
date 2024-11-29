d4ba0f62c6b1a975663dc1fb1746382f34ba00b2
TheValour <helloworldexe18@gmail.com>
2024-11-29 12:16:43 +0530

    basic UI

--- frontend/src/components/Features/Card/Card.jsx
+++ frontend/src/components/Features/Card/Card.jsx
@@ -1,17 +1,16 @@
 import './Styles/Card.css'; 
 
 const Card = ({ icon, title, description }) => {
+  console.log(icon);
   return (
-    <>
     <div className="card">
     
-      <div className="icon-container">
+      <div className="icon-container FLEX">
         <img src={icon} alt={title} />
       </div>
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
-    </>
   );
 };
 
