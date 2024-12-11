dc3161db89e80f896f22e21dd91039753efa505c
Shailendra <shailendrabisht69@gmail.com>
2024-12-11 19:01:03 +0530

    For payment related issue

--- frontend/src/components/booking/Booking.jsx
+++ frontend/src/components/booking/Booking.jsx
@@ -84,7 +84,7 @@ export default function Booking() {
         setPaymentBy(amountInDigits);   
 
         try {
-            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}`/orders, {
+            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/orders`, {
                 amount: amountInDigits * 100,
                 currency: "INR"
             }, {
